612e82c8050dc8f5945380ae3addcb07c56838e5
LouisNgatale <louisngatale@gmail.com>
2021-04-27 21:49:00 +0300

    Temperature component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
     <Header/>
-    
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <Temperature/>
+        </div>
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Temperature from './components/Temperature.vue'
 
 export default {
-  components: { Header },
+  components: { Header, Temperature },
   name: 'App'
 }
 </script>
--- src/components/Temperature.vue
+++ src/components/Temperature.vue
@@ -1,1 +1,78 @@
-template
+<template>
+    <div class="widget">
+        <div class="p-2">
+            <span class="banner">Temperature</span>
+        </div>
+        <div class="mx-2 my-2 icon">
+            <img src="@/assets/icons/temperature.svg" alt="">
+        </div>
+        <div class="mx-2 my-2 container-fluid">
+            <div class="row">
+                <div class="col">
+                    <p class="sub-title">Warehouse</p>
+                    <p class="value">25&#176;C</p>
+                </div>
+                <div class="col">1</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+p{
+    margin: 0;
+}
+.widget{
+    height: 250px;
+    width: 240px;
+    border-radius: 19px;
+    border: 1px $orange solid;
+    background-color: rgb(6,6,9);
+    background-color: linear-gradient(39deg, rgba(6,6,9,1) 0%, rgba(14,18,36,1) 100%);
+    transition: all .4s;
+    transition: background-color 0.5s ease;
+    padding: 10px 10px;
+    &:hover{
+        cursor: pointer;
+        background: rgb(8,8,9);
+        background-color: linear-gradient(39deg, rgba(8,8,9,1) 0%, rgba(15,12,10,1) 100%);
+        box-shadow: 0px 4px 40px rgba($color: #BC7F16, $alpha: .3);
+        transition: all .4s;
+    }
+
+    .banner{
+        border: 1px $orange solid;
+        border-radius: 5px;
+        padding: 5px 8px;
+        font-size: 12px;
+        background: #0C0F1C;
+    }
+
+    .icon{
+        display: flex;
+        justify-content: center;
+        align-content: center;
+    }
+
+    .icon img{
+        margin: auto;
+        display: inline-block;
+    }
+    .sub-title{
+        color: $grey;
+        font-size: 15px;
+    }
+    .value{
+        color: $grey;
+        font-size: 40px;
+        font-weight: bold;
+    }
+}
+
+</style>
